d0d81673032fb1396b962980bc5e682c36d44fb2
Mona Lisa <kseniabelikova35@gmail.com>
2021-02-08 20:04:37 +0300

    .

--- lesson2/task2/src/index.js
+++ lesson2/task2/src/index.js
@@ -25,7 +25,7 @@ resetBtn.addEventListener("click", resetHandler);
 store.subscribe(() => {
   const state = store.getState();
   const currentValue = state.history.reduce((acc, value) => acc + +value, 0);
-  const historyString = state.history.join(" ");
+  const historyString = state.history.join("");
 
   resultEl.textContent =
     state.history.length === 0 ? "" : `${historyString} = ${currentValue}`;
--- lesson2/task2/src/store.js
+++ lesson2/task2/src/store.js
@@ -33,6 +33,7 @@ export const counterReducer = (state = initialState, action) => {
       };
     case DECREMENT:
       return {
+        ...state,
         history: state.history.concat(-1),
       };
     case RESET:
