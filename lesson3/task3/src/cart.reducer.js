8134dd455a217a58d5551d9b287112f7c49c40a9
Mona Lisa <kseniabelikova35@gmail.com>
2021-02-08 22:45:20 +0300

    structure of store

--- lesson3/task3/src/cart.reducer.js
+++ lesson3/task3/src/cart.reducer.js
@@ -9,7 +9,7 @@ const cartReducer = (state = initialState, action) => {
     case ADD_PRODUCT: {
       return {
         ...state,
-        products: state.products.concat(action.payload),
+        products: state.products.concat(action.payload.productData),
       };
     }
     case REMOVE_PRODUCT: {
