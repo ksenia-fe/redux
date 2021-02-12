ef246f3a0ef3ddb9abbe8da0ee3b9974ffccc29e
Mona Lisa <kseniabelikova35@gmail.com>
2021-02-12 15:22:28 +0300

    6.2

--- lesson6/task2/src/store.js
+++ lesson6/task2/src/store.js
@@ -2,12 +2,24 @@ import { createStore, compose, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import userReducer from "./users/users.reducer";
 
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.info("daispatching", action);
+  const result = next(action);
+  console.log("next state", store.getState());
+  console.groupEnd();
+  return result;
+};
+
 const reducer = combineReducers({
   users: userReducer,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
 
 export default store;
